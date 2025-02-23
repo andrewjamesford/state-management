30c3092fa6472bca6b919b995ba9265d1d1833e8
Andrew Ford <andrew@andrewford.co.nz>
2025-02-23 20:05:42 +1300

    refactor: Replace form fields with ListingForm component for improved structure and maintainability

--- client/src/forms/listingForm.tsx
+++ client/src/forms/listingForm.tsx
@@ -1,3 +1,418 @@
-export default function ListingForm() {
-	return <></>;
+// import { useActionState } from "react";
+
+import { type Listing, type Category, listingSchema } from "~/models";
+import RadioButton from "~/components/radioButton";
+import DateInput from "~/components/dateInput";
+import TextInput from "~/components/textInput";
+import Select from "~/components/select";
+import Textarea from "~/components/textarea";
+import MoneyTextInput from "~/components/moneyTextInput";
+import Checkbox from "~/components/Checkbox";
+import SubmitButton from "~/components/submitButton";
+import Loader from "~/components/loader";
+import { format } from "date-fns/format";
+
+export default function ListingForm({
+	listingId: number,
+	formState,
+	setFormState,
+	today,
+	tomorrow,
+	fortnight,
+	loadingCategory,
+	loadingSubCategory,
+	categoryData,
+	subCategoryData,
+}) {
+	return (
+		<>
+			<h1 className="mt-4 text-2xl font-bold">What are you listing?</h1>
+
+			<div className="mt-6">
+				{/* Listing Title */}
+				<TextInput
+					labelClassName="block text-sm font-medium text-gray-700"
+					label="Listing title"
+					id="listing-title"
+					placeholder="e.g. iPhone 5c, Red t-shirt"
+					value={formState.title}
+					onChange={(e) => {
+						const value = e.target.value ?? "";
+						setFormState((prev) => ({
+							...prev,
+							title: value,
+						}));
+					}}
+					required={true}
+					maxLength={80}
+					minLength={3}
+					className="peer mt-1 block w-full rounded-md border px-3 py-2 placeholder:italic invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-600"
+					errorMessage="Please enter a listing title of 3-80 characters"
+					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block"
+					requirementsLabel="80 characters max"
+					requirementsClassName="mt-1 text-sm text-gray-500"
+				/>
+			</div>
+
+			<div className="mt-6">
+				{/* Subtitle */}
+				<TextInput
+					labelClassName="block text-sm font-medium text-gray-700"
+					label="Subtitle (optional)"
+					id="sub-title"
+					placeholder="e.g. iPhone 5c, Red t-shirt"
+					value={formState.subTitle}
+					onChange={(e) => {
+						const value = e.target.value ?? "";
+						setFormState((prev) => ({
+							...prev,
+							subTitle: value,
+						}));
+					}}
+					maxLength={50}
+					className="block w-full px-3 py-2 mt-1 border rounded-md placeholder:italic peer"
+					errorMessage="Please enter a listing title of 3-80 characters"
+					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block"
+					requirementsLabel="50 characters max"
+					requirementsClassName="mt-1 text-sm text-gray-500"
+				/>
+			</div>
+
+			<div className="mt-6">
+				{/* Category */}
+				{loadingCategory && <Loader width={20} height={20} />}
+
+				{!loadingCategory && (
+					<Select
+						label="Category"
+						labelClassName="block text-sm font-medium text-gray-700"
+						id="category"
+						selectClassName={`block w-full h-10 px-3 py-2 items-center justify-between rounded-md border border-input bg-background ring-offset-background peer ${formState.categoryId === 0 ? " italic text-gray-400" : ""}`}
+						onChange={(e) => {
+							const value = Number.parseInt(e.target.value) || 0;
+							setFormState((prev) => ({
+								...prev,
+								categoryId: value,
+								subCategoryId: 0,
+							}));
+						}}
+						value={formState.categoryId}
+						required={true}
+						options={[
+							{
+								value: 0,
+								label: "Select a category...",
+								className: "text-muted-foreground italic",
+							},
+							...(categoryData ?? []).map((category: Category) => ({
+								value: category.id,
+								label: category.category_name,
+							})),
+						]}
+					/>
+				)}
+			</div>
+
+			<div className="mt-6">
+				{/* Sub Category */}
+				{loadingSubCategory && <Loader width={20} height={20} />}
+
+				{!loadingSubCategory && (
+					<Select
+						label="Sub Category"
+						labelClassName="block text-sm font-medium text-gray-700"
+						id="category-sub"
+						selectClassName={`block w-full h-10 px-3 py-2 items-center justify-between rounded-md border border-input bg-background ring-offset-background peer ${formState.subCategoryId === 0 ? " italic text-gray-400" : ""}`}
+						onChange={(e) => {
+							const value = Number.parseInt(e.target.value) || 0;
+							setFormState((prev) => ({
+								...prev,
+								titleCategory: { ...prev, subCategoryId: value },
+							}));
+						}}
+						value={formState.subCategoryId}
+						required={true}
+						disabled={!subCategoryData}
+						options={[
+							{
+								value: 0,
+								label: "Select a sub category...",
+								className: "text-muted-foreground italic",
+							},
+							...(subCategoryData ?? []).map((category: Category) => ({
+								value: category.id,
+								label: category.category_name,
+							})),
+						]}
+					/>
+				)}
+			</div>
+
+			<div className="mt-6">
+				{/* End Date */}
+				<DateInput
+					label="End date"
+					labelClassName="block text-sm font-medium text-gray-700"
+					id="end-date"
+					value={format(formState.endDate, "yyyy-MM-dd")}
+					onChange={(e) =>
+						setFormState((prev) => ({
+							...prev,
+							endDate: e.target.value,
+						}))
+					}
+					required
+					pattern="\d{4}-\d{2}-\d{2}"
+					min={tomorrow}
+					max={fortnight}
+					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:default):invalid]:block"
+					error="Please select a future date between tomorrow and two weeks from now"
+				/>
+			</div>
+
+			<h1 className="mt-4 text-2xl font-bold">Item details</h1>
+			<div className="mt-6">
+				{/* Description */}
+				<Textarea
+					label="Description"
+					labelClassName="block text-sm font-medium text-gray-700"
+					id="listing-description"
+					value={formState.description}
+					onChange={(e) => {
+						const value = e.target.value ?? "";
+						setFormState((prev) => ({
+							...prev,
+							description: value,
+						}));
+					}}
+					required={true}
+					maxLength={500}
+					minLength={10}
+					className="block w-full px-3 py-2 mt-1 border rounded-md placeholder:italic"
+					placeholder="Describe your item"
+					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block"
+					errorMessage="Please enter a description of 10-500 characters"
+				/>
+			</div>
+			<fieldset>
+				<legend className="sr-only">Condition</legend>
+				<div className="mt-6">
+					{/* New & Used Condition */}
+					<label
+						htmlFor="condition"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Condition
+					</label>
+					<RadioButton
+						id="used"
+						name="condition"
+						value="false"
+						label="Used"
+						checked={formState.condition === false}
+						onChange={() =>
+							setFormState((prev) => ({
+								...prev,
+								condition: false,
+							}))
+						}
+						labelClassName="ml-2 text-sm text-gray-700"
+						containerClassName="flex mt-3"
+					/>
+					<RadioButton
+						id="new"
+						name="condition"
+						value="true"
+						label="New"
+						checked={formState.condition === true}
+						onChange={() =>
+							setFormState((prev) => ({
+								...prev,
+								condition: true,
+							}))
+						}
+						labelClassName="ml-2 text-sm text-gray-700"
+						containerClassName="flex mt-3"
+					/>
+				</div>
+			</fieldset>
+
+			<h1 className="mt-4 text-2xl font-bold">Price &amp; Payment</h1>
+			<div className="mt-6">
+				{/* Start price */}
+				<MoneyTextInput
+					label="Start price"
+					labelClassName="block text-sm font-medium text-gray-700"
+					id="listing-price"
+					placeholder="$10.00"
+					value={formState.listingPrice}
+					onChange={(e) => {
+						setFormState((prev) => ({
+							...prev,
+							listingPrice: Number.parseFloat(e.target.value),
+						}));
+					}}
+				/>
+			</div>
+			<div className="mt-6">
+				{/* Reserve price */}
+				<MoneyTextInput
+					label="Reserve price (optional)"
+					labelClassName="block text-sm font-medium text-gray-700"
+					id="listing-reserve"
+					placeholder="$20.00"
+					value={formState.reservePrice}
+					onChange={(e) => {
+						setFormState((prev) => ({
+							...prev,
+							reservePrice: Number.parseFloat(e.target.value),
+						}));
+					}}
+				/>
+			</div>
+			<fieldset>
+				<legend className="sr-only">Payment options</legend>
+				<div className="mt-6">
+					{/* Payment options */}
+					<label
+						htmlFor="category"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Payment options
+					</label>
+					<div className="flex mt-3">
+						<Checkbox
+							id="payment-credit"
+							label="Credit card"
+							checked={formState.creditCardPayment}
+							onChange={() => {
+								setFormState((prev) => ({
+									...prev,
+									creditCardPayment: !prev.creditCardPayment,
+								}));
+							}}
+						/>
+					</div>
+					<div className="flex mt-3">
+						<Checkbox
+							id="payment-bank"
+							label="Bank Transfer"
+							checked={formState.bankTransferPayment}
+							onChange={() => {
+								setFormState((prev) => ({
+									...prev,
+									bankTransferPayment: !prev.bankTransferPayment,
+								}));
+							}}
+						/>
+					</div>
+					<div className="flex mt-3">
+						<Checkbox
+							id="payment-bitcoin"
+							label="Bitcoin"
+							checked={formState.bitcoinPayment}
+							onChange={() => {
+								setFormState((prev) => ({
+									...prev,
+									bitcoinPayment: !prev.bitcoinPayment,
+								}));
+							}}
+						/>
+					</div>
+				</div>
+			</fieldset>
+
+			<h1 className="mt-4 text-2xl font-bold">Shipping & pick-up</h1>
+			<fieldset>
+				<legend className="sr-only">Pick up?</legend>
+				{/* Pick up  */}
+				<div className="mt-6">
+					<label
+						htmlFor="pick-up"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Pick up?
+					</label>
+					<RadioButton
+						id="pick-up-true"
+						name="pick-up"
+						value="true"
+						label="Yes"
+						checked={formState.pickUp === true}
+						onChange={() =>
+							setFormState((prev) => ({
+								...prev,
+								pickUp: true,
+							}))
+						}
+						containerClassName="flex mt-3"
+						labelClassName="ml-2 text-sm text-gray-700"
+					/>
+					<RadioButton
+						id="pick-up-false"
+						name="pick-up"
+						value="false"
+						label="No"
+						checked={formState.pickUp === false}
+						onChange={() =>
+							setFormState((prev) => ({
+								...prev,
+								pickUp: false,
+							}))
+						}
+						containerClassName="flex mt-3"
+						labelClassName="ml-2 text-sm text-gray-700"
+					/>
+				</div>
+			</fieldset>
+
+			<fieldset>
+				<legend className="sr-only">Shipping options</legend>
+				{/* Shipping options */}
+				<div className="mt-6">
+					<label
+						htmlFor="shipping-option"
+						className="block text-sm font-medium text-gray-700"
+					>
+						Shipping options
+					</label>
+					<RadioButton
+						id="shipping-option-courier"
+						name="shipping-option"
+						value="courier"
+						label="Courier"
+						checked={formState.shippingOption === "courier"}
+						onChange={() =>
+							setFormState((prev) => ({
+								...prev,
+								shippingOption: "courier",
+							}))
+						}
+						containerClassName="flex mt-3"
+						labelClassName="ml-2 text-sm text-gray-700"
+					/>
+					<RadioButton
+						id="shipping-option-post"
+						name="shipping-option"
+						value="post"
+						label="Post"
+						checked={formState.shippingOption === "post"}
+						onChange={() =>
+							setFormState((prev) => ({
+								...prev,
+								shippingOption: "post",
+							}))
+						}
+						containerClassName="flex mt-3"
+						labelClassName="ml-2 text-sm text-gray-700"
+					/>
+				</div>
+				<input type="hidden" name="id" value={listingId} />
+			</fieldset>
+
+			<div className="mt-3">
+				<SubmitButton />
+			</div>
+		</>
+	);
 }
--- client/src/routes/tsquery/add.tsx
+++ client/src/routes/tsquery/add.tsx
@@ -6,13 +6,8 @@ import type { Listing, ListingSchema } from "~/models";
 import { listingSchema } from "~/models";
 import Loader from "~/components/loader";
 import api from "~/api";
-import RadioButton from "~/components/radioButton";
-import DateInput from "~/components/dateInput";
-import TextInput from "~/components/textInput";
-import Select from "~/components/select";
-import Textarea from "~/components/textarea";
-import MoneyTextInput from "~/components/moneyTextInput";
-import Checkbox from "~/components/Checkbox";
+import ListingForm from "~/forms/listingForm";
+import ErrorMessage from "~/components/errorMessage";
 
 export const Route = createFileRoute("/tsquery/add")({
 	component: RouteComponent,
@@ -37,9 +32,9 @@ function RouteComponent() {
 	const [formState, setFormState] = useState(listingSchema);
 
 	const {
-		data: parentCatData,
+		data: categoryData,
 		isLoading: loadingCategory,
-		error: parentError,
+		error: categoryError,
 	} = useQuery({
 		queryKey: ["parentCategories"],
 		queryFn: async () => {
@@ -51,9 +46,9 @@ function RouteComponent() {
 	});
 
 	const {
-		data: subCatData,
+		data: subCategoryData,
 		isLoading: loadingSubCategory,
-		error: subCatError,
+		error: subCategoryError,
 	} = useQuery({
 		queryKey: ["subCategories", formState.categoryId],
 		queryFn: async () => {
@@ -106,360 +101,25 @@ function RouteComponent() {
 		return navigate({ to: "/tsquery" });
 	};
 
-	if (parentError) return <p>Error: {parentError.message}</p>;
-	if (subCatError) return <p>Error: {subCatError.message}</p>;
-	if (loadingCategory) return <Loader height={50} width={50} />;
+	if (categoryError)
+		return <ErrorMessage message="Error: Error loading Categories" />;
+	if (subCategoryError)
+		return <ErrorMessage message="Error: Error loading Sub-Categories" />;
 
 	return (
 		<form onSubmit={handleSubmit} noValidate className="group">
-			<h1 className="mt-4 text-2xl font-bold">What are you listing?</h1>
-
-			<div className="mt-6">
-				{/* Listing Title */}
-				<TextInput
-					labelClassName="block text-sm font-medium text-gray-700"
-					label="Listing title"
-					id="listing-title"
-					placeholder="e.g. iPhone 5c, Red t-shirt"
-					value={formState.title}
-					onChange={(e) => {
-						const value = e.target.value ?? "";
-						setFormState((prev) => ({
-							...prev,
-							title: value,
-						}));
-					}}
-					required={true}
-					maxLength={80}
-					minLength={3}
-					className="peer mt-1 block w-full rounded-md border px-3 py-2 placeholder:italic invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-600"
-					errorMessage="Please enter a listing title of 3-80 characters"
-					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block"
-					requirementsLabel="80 characters max"
-					requirementsClassName="mt-1 text-sm text-gray-500"
-				/>
-			</div>
-
-			<div className="mt-6">
-				{/* Subtitle */}
-				<TextInput
-					labelClassName="block text-sm font-medium text-gray-700"
-					label="Subtitle (optional)"
-					id="category-sub"
-					placeholder="e.g. iPhone 5c, Red t-shirt"
-					value={formState.title}
-					onChange={(e) => {
-						const value = e.target.value ?? "";
-						setFormState((prev) => ({
-							...prev,
-							title: value,
-						}));
-					}}
-					required={false}
-					maxLength={50}
-					className="block w-full px-3 py-2 mt-1 border rounded-md placeholder:italic peer"
-					errorMessage="Please enter a listing title of 3-80 characters"
-					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block"
-					requirementsLabel="50 characters max"
-					requirementsClassName="mt-1 text-sm text-gray-500"
-				/>
-			</div>
-
-			<div className="mt-6">
-				{/* Category */}
-				{loadingCategory && <Loader width={20} height={20} />}
-				{!loadingCategory && (
-					<Select
-						label="Category"
-						labelClassName="block text-sm font-medium text-gray-700"
-						id="category"
-						selectClassName={`block w-full h-10 px-3 py-2 items-center justify-between rounded-md border border-input bg-background ring-offset-background peer ${formState.categoryId === 0 ? " italic text-gray-400" : ""}`}
-						onChange={(e) => {
-							const value = Number.parseInt(e.target.value) || 0;
-							setFormState((prev) => ({
-								...prev,
-								categoryId: value,
-								subCategoryId: 0, // reset subcategory
-							}));
-						}}
-						value={formState.categoryId}
-						required={true}
-					>
-						<option value={0} className="text-muted-foreground italic">
-							Select a category...
-						</option>
-						{parentCatData?.map((category: Category) => (
-							<option key={category.id} value={category.id}>
-								{category.category_name}
-							</option>
-						))}
-					</Select>
-				)}
-			</div>
-
-			<div className="mt-6">
-				{/* Sub Category */}
-				{loadingSubCategory && <Loader width={20} height={20} />}
-				<Select
-					label="Sub Category"
-					labelClassName="block text-sm font-medium text-gray-700"
-					id="category-sub"
-					selectClassName={`block w-full h-10 px-3 py-2 items-center justify-between rounded-md border border-input bg-background ring-offset-background peer ${formState.subCategoryId === 0 ? " italic text-gray-400" : ""}`}
-					onChange={(e) => {
-						const value = Number.parseInt(e.target.value) || 0;
-						setFormState((prev) => ({
-							...prev,
-							subCategoryId: value,
-						}));
-					}}
-					value={formState.subCategoryId}
-					required={true}
-					disabled={!subCatData}
-				>
-					<option value={0} className="text-muted-foreground italic">
-						Select a sub category...
-					</option>
-					{subCatData?.map((category: Category) => (
-						<option key={category.id} value={category.id}>
-							{category.category_name}
-						</option>
-					))}
-				</Select>
-			</div>
-
-			<div className="mt-6">
-				{/* End date */}
-				<DateInput
-					label="End date"
-					labelClassName="block text-sm font-medium text-gray-700"
-					id="end-date"
-					value={formState.endDate}
-					onChange={(e) => {
-						const value = e.target.value ?? tomorrow;
-						setFormState((prev) => ({
-							...prev,
-							endDate: value,
-						}));
-					}}
-					required
-					pattern="\d{4}-\d{2}-\d{2}"
-					min={tomorrow}
-					max={fortnight}
-					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:default):invalid]:block"
-					error="Please select a future date between tomorrow and two weeks from now"
-				/>
-			</div>
-
-			<h1 className="mt-4 text-2xl font-bold">Item details</h1>
-			<div className="mt-6">
-				{/* Description */}
-				<Textarea
-					label="Description"
-					labelClassName="block text-sm font-medium text-gray-700"
-					id="listing-description"
-					value={formState.description}
-					onChange={(e) => {
-						const value = e.target.value ?? "";
-						setFormState((prev) => ({
-							...prev,
-							description: value,
-						}));
-					}}
-					required={true}
-					maxLength={500}
-					minLength={10}
-					className="block w-full px-3 py-2 mt-1 border rounded-md placeholder:italic"
-					placeholder="Describe your item"
-					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block"
-					errorMessage="Please enter a description of 10-500 characters"
-				/>
-			</div>
-			<fieldset>
-				<legend className="sr-only">Condition</legend>
-				<div className="mt-6">
-					<label
-						htmlFor="condition"
-						className="block text-sm font-medium text-gray-700"
-					>
-						Condition
-					</label>
-					<RadioButton
-						id="used"
-						name="condition"
-						value="false"
-						label="Used"
-						checked={formState.condition === false}
-						onChange={() =>
-							setFormState((prev) => ({
-								...prev,
-								condition: false,
-							}))
-						}
-						labelClassName="ml-2 text-sm text-gray-700"
-						containerClassName="flex mt-3"
-					/>
-					<RadioButton
-						id="new"
-						name="condition"
-						value="true"
-						label="New"
-						checked={formState.condition === true}
-						onChange={() =>
-							setFormState((prev) => ({
-								...prev,
-								condition: true,
-							}))
-						}
-						labelClassName="ml-2 text-sm text-gray-700"
-						containerClassName="flex mt-3"
-					/>
-				</div>
-			</fieldset>
-
-			<h1 className="mt-4 text-2xl font-bold">Price &amp; Payment</h1>
-			<div className="mt-6">
-				<MoneyTextInput
-					label="Start price"
-					labelClassName="block text-sm font-medium text-gray-700"
-					id="listing-price"
-					placeholder="$10.00"
-					value={formState.listingPrice}
-					onChange={(e) => {
-						checkValue(Number(e.target.value));
-						setFormState((prev) => ({
-							...prev,
-							listingPrice: Number(e.target.value),
-						}));
-					}}
-					required={true}
-					errorMessage="Price must be less than $10"
-					errorClassName="mt-1 hidden text-sm text-red-600 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block"
-				/>
-			</div>
-			<div className="mt-6">
-				<MoneyTextInput
-					label="Reserve price (optional)"
-					labelClassName="block text-sm font-medium text-gray-700"
-					id="listing-reserve"
-					placeholder="$20.00"
-					value={formState.reservePrice}
-					onChange={(e) => {
-						setFormState((prev) => ({
-							...prev,
-							reservePrice: Number(e.target.value),
-						}));
-					}}
-				/>
-			</div>
-			<fieldset>
-				<legend className="sr-only">Payment options</legend>
-				<div className="mt-6">
-					<label
-						htmlFor="category"
-						className="block text-sm font-medium text-gray-700"
-					>
-						Payment options
-					</label>
-					<div className="flex mt-3">
-						<Checkbox
-							id="payment-credit"
-							label="Credit card"
-							checked={formState.creditCardPayment}
-							onChange={() =>
-								setFormState((prev) => ({
-									...prev,
-									creditCardPayment: !prev.creditCardPayment,
-								}))
-							}
-						/>
-					</div>
-					<div className="flex mt-3">
-						<Checkbox
-							id="payment-bank"
-							label="Bank Transfer"
-							checked={formState.bankTransferPayment}
-							onChange={() =>
-								setFormState((prev) => ({
-									...prev,
-									bankTransferPayment: !prev.bankTransferPayment,
-								}))
-							}
-						/>
-					</div>
-					<div className="flex mt-3">
-						<Checkbox
-							id="payment-bitcoin"
-							label="Bitcoin"
-							checked={formState.bitcoinPayment}
-							onChange={() =>
-								setFormState((prev) => ({
-									...prev,
-									bitcoinPayment: !prev.bitcoinPayment,
-								}))
-							}
-						/>
-					</div>
-				</div>
-			</fieldset>
-
-			<h1 className="mt-4 text-2xl font-bold">Shipping & pick-up</h1>
-			<fieldset>
-				<legend className="sr-only">Pick up?</legend>
-				<div className="mt-6">
-					<label
-						htmlFor="pick-up"
-						className="block text-sm font-medium text-gray-700"
-					>
-						Pick up?
-					</label>
-					<RadioButton
-						id="pick-up-true"
-						name="pick-up"
-						value="true"
-						label="Yes"
-						checked={formState.pickUp === true}
-						onChange={() =>
-							setFormState((prev) => ({
-								...prev,
-								pickUp: true,
-							}))
-						}
-						containerClassName="flex mt-3"
-						labelClassName="ml-2 text-sm text-gray-700"
-					/>
-					<RadioButton
-						id="pick-up-false"
-						name="pick-up"
-						value="false"
-						label="No"
-						checked={formState.pickUp === false}
-						onChange={() =>
-							setFormState((prev) => ({
-								...prev,
-								pickUp: false,
-							}))
-						}
-						containerClassName="flex mt-3"
-						labelClassName="ml-2 text-sm text-gray-700"
-					/>
-				</div>
-			</fieldset>
-
-			{/* Removed: hidden input for listingId */}
-			<div className="mt-3">
-				<SubmitButton />
-			</div>
+			<ListingForm
+				listingId={0}
+				formState={formState}
+				setFormState={setFormState}
+				today={today}
+				tomorrow={tomorrow}
+				fortnight={fortnight}
+				loadingCategory={loadingCategory}
+				loadingSubCategory={loadingSubCategory}
+				categoryData={categoryData}
+				subCategoryData={subCategoryData}
+			/>
 		</form>
-	);
-}
-
-function SubmitButton() {
-	return (
-		<button
-			type="submit"
-			className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-blue-500 hover:bg-blue-600 text-white border border-blue-600 h-10 px-4 py-2"
-		>
-			Save
-		</button>
 	);
 }
